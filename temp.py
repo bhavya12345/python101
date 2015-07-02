6646187af7ace279fbad79dae076d9b86b01a5d5
Yask Srivastava <yask123@gmail.com>
2015-07-02 21:32:23 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,3 +1,3 @@
-link = 'http://songscloud.in/music/down/12105501/2435472/YTg0M2tCNm4zaDl1UnZnem9OT0h2UWhHZWFJWXFnY0ZtK1pNVmxSZXFLTjd0aUt4eGc=/Humdard+-+Arijit+Singh.mp3'
+link = 'wget http://songscloud.in/music/down/12105501/2435472/YTg0M2tCNm4zaDl1UnZnem9OT0h2UWhHZWFJWXFnY0ZtK1pNVmxSZXFLTjd0aUt4eGc=/Humdard+-+Arijit+Singh.mp3'
 import os
-os.system('link')
+os.system(link)
